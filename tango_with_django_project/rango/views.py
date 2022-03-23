33f70ae31ee37f14d432e686fe34fe9e51d3d7ce
AOtter24 <aotts10@yahoo.co.uk>
2022-03-23 15:49:29 +0000

    Chapter 3

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About Page</a>")
+
+
+def about(request):
+    return HttpResponse("This is the about page :) <a href='/rango/'>Index Page</a>")
