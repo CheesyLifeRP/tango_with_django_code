5888cc493727a95acb76df6aeb9c2b9cc872468f
AOtter24 <aotts10@yahoo.co.uk>
2022-03-28 13:26:57 +0100

    Chapter 6 complete

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rango import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
+    # path('add_category/', views.add_category, name='add_category'),
+    # path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
+]
